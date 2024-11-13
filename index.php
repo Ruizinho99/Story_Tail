48bb59295c5bbb89b9a27d851de576c623d43bc5
Rui Lopes <ruijorgelopes229@gmail.com>
2024-11-13 23:05:25 +0000

    Try push

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo com iframe</title>
+</head>
+<body>
+    <?php include 'header.html';?>
+    <h1>Página Principal</h1>
+
+
+
+
+
+
+
+
+
+
+    <?php include 'footer.html'; ?>
+    
+</body>
+</html>
