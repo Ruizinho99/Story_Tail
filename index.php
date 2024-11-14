6bda152a989d80b32883709a0d636b2a5330cd81
Rui Lopes <ruijorgelopes229@gmail.com>
2024-11-14 00:27:17 +0000

    Search Bar on Index

--- index.php
+++ index.php
@@ -3,18 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     <title>Exemplo com iframe</title>
 </head>
 <body>
     <?php include 'header.html';?>
-    <h1>Página Principal</h1>
 
-
-
-
-
-
-
+    <section class="search-section">
+    <div class="search-container">
+        <h2>Find a book</h2>
+        <div class="search-bar">
+            <input type="text" placeholder="eg. title, type..." />
+            <button type="submit">
+                <span class="material-icons">search</span>
+            </button>
+        </div>
+    </div>
+</section>
 
 
 
