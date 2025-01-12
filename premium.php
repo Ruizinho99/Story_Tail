3c29f8f02f63c2845bbb3db4ae33cc0ac8096f0a
ruben-af <rubenafonso094@gmail.com>
2025-01-12 16:02:27 +0000

    premium css

--- premium.php
+++ premium.php
@@ -37,6 +37,7 @@ $result = $conn->query($sql);
     <title>Admin - Solicitações de Plano Premium</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="Styles/premium.css">
 </head>
 <body>
     <div class="container py-5">
